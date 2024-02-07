df550b410853914bf3335e0699dd80f484f2561b
KZHiganov <kzhiganov@zvezdamedia.ru>
2024-02-07 10:36:38 +0300

    Сделал пока визуально компонент фильтр

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import { filterMock } from '../../libs/constants/mock/filter.mock';
+'use client';
+
+import React, { useState } from 'react';
+import { filterMock } from 'libs/constants/mock/filter.mock';
+import styles from './Filter.module.scss';
+import { clsx } from 'clsx';
 
 export const Filter = () => {
+  const [typeInfo, setTypeInfo] = useState('Телеканал Звезда');
+
+  const clickButton = (type) => {
+    setTypeInfo(type);
+  };
+
   return (
-    <article>
-      <h2>Фильтр</h2>
-      <nav>
+    <article className={styles.container}>
+      <h3 className={styles.filter}>Фильтр</h3>
+      <nav className={styles.navigation}>
         {filterMock.map((item) => {
-          return <button>{item}</button>;
+          return (
+            <button
+              onClick={() => {
+                clickButton(item);
+              }}
+              className={clsx(styles.filterButton, {
+                [styles.active]: typeInfo === item,
+              })}
+            >
+              {item}
+            </button>
+          );
         })}
       </nav>
     </article>
